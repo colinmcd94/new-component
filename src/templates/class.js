9a575fe52f39673d02e449a0138c4ea1053197b2
Colin McDonnell <colinmcd@alum.mit.edu>
2018-02-20 01:37:14 -0500

    Implemented css file generation

--- src/templates/class.js
+++ src/templates/class.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import "./COMPONENT_NAME.css";
 
 class COMPONENT_NAME extends Component {
   render() {
--- src/templates/pure-class.js
+++ src/templates/pure-class.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-
+import React, { PureComponent } from "react";
+import "./COMPONENT_NAME.css";
 
 class COMPONENT_NAME extends PureComponent {
   render() {
